221f198e11c0048e1ec0b3f8304f0a3fd31c794b
MeUuS <donghwi.lee@shareableasset.com>
2021-06-29 14:15:45 +0900

    Update main.js

--- Test_MQTT/main.js
+++ Test_MQTT/main.js
@@ -2,28 +2,28 @@ const mqtt = require('mqtt')
 // const client = mqtt.connect('mqtt://test.mosquitto.org')
 const client = mqtt.connect('tcp://test.mosquitto.org:1883')
 const TOPIC_NAME = "DH_MQTT_Test"
-const OPTION_INTERVAL_START = "INTERVAL_START"
-const OPTION_INTERVAL_END = "INTERVAL_END"
+const OPTION_INTERVAL_START = '[Op01]'
+const OPTION_INTERVAL_END = '[Op02]'
 
-client.subscribe(TOPIC_NAME);
+client.subscribe(TOPIC_NAME)
 client.on('message', function (topic, message) {
-    console.log("topic : " + topic.toString() + "\nmessage : " + message.toString())
-    if (message.toString().match(OPTION_INTERVAL_START))
+    console.log("topic : " + topic.toString() + " message : " + message.toString())
+    if (message.toString().startsWith(OPTION_INTERVAL_START))
         startInterval()
-    else if (message.toString().match(OPTION_INTERVAL_END))
+    else if (message.toString().startsWith(OPTION_INTERVAL_END))
         endInterval()
 })
 
-var timerId = null;
+var timerId = null
 function startInterval() {
     if (timerId == null)
         timerId = setInterval(
             () => {
-                let rndStr = Math.random().toString(36).substr(2, 11);
-                client.publish(TOPIC_NAME, "server : " + rndStr);
+                let rndStr = Math.random().toString(36).substr(2, 11)
+                client.publish(TOPIC_NAME, "[Server] " + rndStr)
             },
             2000
-        );
+        )
 }
 
 function endInterval() {
